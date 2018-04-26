612fadb2894caaf87771f29b2c39f02499d67a09
Fabiano Moreira <fabiano.moreira.alves@gmail.com>
2018-04-26 12:27:25 -0300

    yourName.py
    While and Break function examples.

---
+++ yourName.py
@@ -0,0 +1,7 @@
+name = ''
+while True: #learning function while
+    print ('Please, type your name.')
+    name = input()
+    if name == 'your name':
+        break # using the function break to stop the loop
+print ('Thanks!')
