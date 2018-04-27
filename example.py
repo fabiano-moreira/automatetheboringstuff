66a681c9a112cf9ea99e6c27841d0f9a43e8d447
Fabiano Moreira <fabiano.moreira.alves@gmail.com>
2018-04-27 12:05:35 -0300

    example.py
    Uses the def statement to define a function call hello;
    Global Variable
    Local Variable

--- example.py
+++ example.py
@@ -20,3 +20,38 @@ print(newNumber)
 
 print ('Hello', end='')
 print ('world')
+
+
+############################
+
+spam = 42 # Global Variable
+
+def eggs():
+    spam = 42 # Local Variable
+
+print ('Some code here.')
+print ('Some more code.')
+
+##############
+
+def spam(): 
+    eggs = 99 # Local Variable
+    bacon()
+    print (eggs)
+
+def bacon():
+    ham = 101
+    eggs = 0 # Local Variable
+
+spam()
+
+#################
+
+def spam(): 
+    eggs = 'Hello world'
+    print (eggs)
+
+
+eggs = 42
+spam()
+print(eggs)
